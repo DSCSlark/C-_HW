d56d343422ecec0ced9fa86445e3a84ebf573491
DSCSlark <9047915555@mail.ru>
2023-03-20 16:07:23 +0300

    hw done

--- 1_Lesson/3/Program.cs
+++ 1_Lesson/3/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно на 2 без остатка).
+
+Console.WriteLine("Please, write a number");
+int n_1 = int.Parse(Console.ReadLine()!);
+
+if (n_1 % 2 == 0)
+{
+    Console.Write($"The number {n_1} is even");
+}
+else
+{
+    Console.Write($"The number {n_1} is odd");
+}
--- 1_Lesson/4/Program.cs
+++ 1_Lesson/4/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные числа от 1 до (N).
+
+Console.WriteLine("Please, write a number: ");
+
+int num = int.Parse(Console.ReadLine()!);
+int i = 2;
+
+while(i <= num)
+{
+    Console.Write($"{i} ");
+    i += 2;
+}
