e9bc07f05eced4e84d5c53229e59f6ba42e2c3a2
DSCSlark <9047915555@mail.ru>
2023-03-20 16:02:50 +0300

    task 1 rdy

---
+++ 1_Lesson/2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+double a = 2;
+double b = 33;
+double c = 7;
+
+double max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
