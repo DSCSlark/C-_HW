d02b3d36b3311d520909d12ef850805733d0e66f
DSCSlark <9047915555@mail.ru>
2023-03-22 13:02:41 +0300

    asdd

---
+++ 2_Lesson/2_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+void ThirdNum(int num)
+{
+    if (num < 100)
+    {
+        Console.WriteLine("There is no 3rd digit");
+        return;
+    }
+
+    while (num > 999) num /= 10;
+    Console.WriteLine(num % 10);
+}
+
+int val = int.Parse(Console.ReadLine()!);
+ThirdNum(val);
