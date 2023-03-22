64716bdb8bd0463893620695a6bf5e48e82cf761
DSCSlark <9047915555@mail.ru>
2023-03-22 12:35:04 +0300

    task 2 done

---
+++ 2_Lesson/2_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа
+
+void NumRendom()
+
+{
+    int num = new Random().Next(100, 1000);
+    Console.WriteLine($"{num} -> {num / 100}{num % 10}");
+}
+
+NumRendom();
+
+// ----------------------------------------
+
+string SecNum (int num)
+{
+    return $"{num} -> {num / 100 * 10} + {num % 10}";
+}
+
+int val = int.Parse(Console.ReadLine()!);
+string result = SecNum(val);
+Console.WriteLine(result);
