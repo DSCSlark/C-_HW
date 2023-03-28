2d5cc14f47f274f3590cd07219f66bdc8febcc9c
DSCSlark <9047915555@mail.ru>
2023-03-28 16:57:22 +0300

    task 3_2, 3_3 done

---
+++ 3_lesson/3_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число (N) и выдает таблицу кубов ичсел от 1 до N.
+
+void S(int n)
+{
+    int a = 1;
+    while (a <= n)
+    {
+        Console.WriteLine(a * a * a);
+        a++;
+    }
+}
+
+Console.WriteLine("Введите число: ");
+S(int.Parse(Console.ReadLine())!);
